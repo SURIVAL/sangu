4e0b1371fc38e5bc88f9b3b971a18e94fac2266e
SURIVAL <31478611+SURIVAL@users.noreply.github.com>
2017-09-06 16:43:28 +0530

    string35.c

---
+++ string35.c
@@ -0,0 +1,15 @@
+# sangu
+include<stdio.h>
+void main()
+{
+    char s[]=”program”;
+    int counter = 0 ;
+    int k;
+    for (k = 0; k < s[i]!= 0; k++)
+    { 
+        counter++;
+    }
+
+    printf( "The number of characters is %d", counter);
+
+    return 0;
